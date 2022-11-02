1fc98b180109e77e0c87e92763dc318514f24668
unknown <ansariarham064@gmail.com>
2022-11-02 13:26:28 +0530

    code fixed for prime

--- primeproblem.js
+++ primeproblem.js
@@ -1,13 +1,27 @@
-function checkprime(num){
+function checkprime(number){
+  let bag=0;
+for(let i=1; i<=number; i++){
+  if(number%i==0)
+  {
+     bag++;
+  }
 
-for(let i=0; i<=num; i++){
-  if(num%i){
-
-return true;
+ }
+  if(bag==2)
+  {
+   return true;
+  }
+   return false;
 }
-return false;
 
-}
-}
+let ans=checkprime(13);
 
-let checkprime(13);
+  if(ans==true)
+  {
+  console.log("Prime Number");
+  }
+  else
+
+  {
+   console.log("Not a Prime Number");
+  }
